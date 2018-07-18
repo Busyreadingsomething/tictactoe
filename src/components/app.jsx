26941f1b95ad3bc1bcce18d47d0da4cd16ae5fe5
Makan Azarshahy <oro1919@gmail.com>
2018-07-18 14:23:42 -0700

    Added basic classes and correct id

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,42 @@
 import React from 'react';
 
+const Square = props => (
+  <div className="space" onClick={props.click}>
+    TESTING
+  </div>
+)
+
+const BoardRow = props => (
+  <div>
+    {
+      props.row.map(space => <Square space={space} click={props.click}/>)
+    }
+  </div>
+);
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: [],
+      board: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ],
     };
+  }
+
+  handleClick() {
+    console.log('hit');
   }
 
   render() {
     return (
-      <div>THIS IS THE BOARD</div>
+      <div>
+        {
+          this.state.board.map(row => <BoardRow row={row} click={() => this.handleClick}/>)
+        }
+      </div>
     );
   }
 }
