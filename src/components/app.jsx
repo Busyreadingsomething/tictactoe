bed54024a2bf155ea9f8db9d22c451ff1ebf4bf0
Makan Azarshahy <oro1919@gmail.com>
2018-07-18 16:15:58 -0700

    Added winning conditions

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const Square = props => (
-  <div className="space" onClick={() => props.click(props.row, props.col)}>
-    <h3 className={`mark ${props.space}`}>{props.space}</h3>
+export const Square = ({ click, row, col, space }) => (
+  <div className="space" onClick={() => click(row, col)}>
+    <h3 className={`mark ${space}`}>{space}</h3>
   </div>
 )
 
@@ -12,6 +12,10 @@ export const BoardRow = props => (
       props.row.map((space, index) => <Square space={space} row={props.index} col={index} click={props.click}/>)
     }
   </div>
+);
+
+export const Winner = ({ winner }) => (
+  <h1>WINNER IS {winner}</h1>
 );
 
 class Board extends React.Component {
@@ -28,12 +32,12 @@ class Board extends React.Component {
     };
   }
 
-  cloneBoard() {
+  cloneBoard = () => {
     const { board } = this.state
     return board.map(row => row.map(space => space));
   }
 
-  isWinner(winner) {
+  isWinner = (winner) => {
     if (winner === 3) {
       return 'X';
     } else if (winner === -3) {
@@ -43,21 +47,48 @@ class Board extends React.Component {
     }
   }
 
-  checkWinner() {
+  checkWinner = () => {
+    const {
+      checkRows,
+      checkCols,
+      checkMajor,
+      checkMinor,
+    } = this;
 
+    const methods = {
+      checkRows,
+      checkCols,
+      checkMajor,
+      checkMinor,
+    };
+
+    const list = Object.keys(methods);
+    let winner = false;
+    
+    for (const method of list) {
+      winner = methods[method]();
+      if (winner) {
+        this.setState({ winner });
+        break;
+      }
+    }
   }
 
-  checkRow(row) {
+  checkRow = (row) => {
     let winner = 0;
 
     for (let col = 0; col < 3; col += 1) {
-      row[col] === 'X' ? winner += 1 : winner -= 1;
+      if (row[col] === 'X') {
+        winner += 1;
+      } else if (row[col] === 'O') {
+        winner -= 1;
+      }
     }
 
     return this.isWinner(winner);
   }
 
-  checkRows() {
+  checkRows = () => {
     for (const row of this.state.board) {
       const winner = this.checkRow(row);
       if (winner) {
@@ -66,60 +97,75 @@ class Board extends React.Component {
     }
   }
 
-  checkCol(col) {
+  checkCol = (col) => {
     let winner = 0;
     const { board } = this.state;
 
     for (let row = 0; row < 3; row += 1) {
-      board[col][row] === 'X' ? winner += 1 : winner -= 1;
+      if (board[row][col] === 'X') {
+        winner += 1;
+      } else if (board[row][col] === 'O') {
+        winner -= 1;
+      }
     }
     return this.isWinner(winner);
   }
 
-  checkCols() {
+  checkCols = () => {
     for (let col = 0; col < 3; col += 1) {
-      this.checkCol(col);
+      let winner = this.checkCol(col);
+      if (winner) {
+        return winner;
+      }
+
     }
   }
 
-  checkMajor() {
+  checkMajor = () => {
     let winner = 0;
     const { board } = this.state;
     let col = 0
     for (let row = 0; row < 3; row += 1) {
-      board[row][col] === 'X' ? winner += 1 : winner -= 1;
+      if (board[row][col] === 'X') {
+        winner += 1;
+      } else if (board[row][col] === 'O') {
+        winner -= 1;
+      }
       col += 1;
     }
 
     return this.isWinner(winner);
   }
 
-  checkMinor() {
+  checkMinor = () => {
     let winner = 0;
     const { board } = this.state;
 
     let col = 2;
     for (let row = 0; row < 3; row += 1) {
-      baord[row][col] === 'X' ? winner += 1 : winner -= 1;
+      if (board[row][col] === 'X') {
+        winner += 1;
+      } else if (board[row][col] === 'O') {
+        winner -= 1;
+      }
       col -= 1;
     }
 
     return this.isWinner(winner);
   }
 
-  makeMove(board, row, col) {
+  makeMove = (board, row, col) => {
     if (board[row][col] === '') {
       const turn = this.state.turn === 'O' ? 'X' : 'O';
       board[row][col] = this.state.turn;
       this.setState({ 
         board,
         turn
-      });
+      }, this.checkWinner);
     }
   }
 
-  handleClick(row, col) {
-    console.log('hit', row, col);
+  handleClick = (row, col) => {
     const board = this.cloneBoard();
     this.makeMove(board, row, col);
   }
@@ -127,12 +173,18 @@ class Board extends React.Component {
   render() {
     return (
       <div className="game">
-        <h1>It is currently {this.state.turn}'s turn</h1>
-        <div className="board">
-          {
-            this.state.board.map((row, index) => <BoardRow row={row} index={index} click={(row, col) => this.handleClick(row, col)}/>)
-          }
-        </div>
+        {
+          this.state.winner
+            ? <Winner winner={this.state.winner} />
+            : (<div>
+                <h1>It is currently {this.state.turn}'s turn</h1>
+                <div className="board">
+                  {
+                    this.state.board.map((row, index) => <BoardRow row={row} index={index} click={this.handleClick}/>)
+                  }
+                </div>
+              </div>)
+        }
       </div>
     );
   }
